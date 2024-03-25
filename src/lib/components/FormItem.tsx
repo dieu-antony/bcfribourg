30a6c0eafdec3e17d51fd0cd96686d42fa484d8c
dieu-antony <antonydieu@outlook.com>
2024-03-25 23:07:09 +0100

    updated contact form

--- src/lib/components/FormItem.tsx
+++ src/lib/components/FormItem.tsx
@@ -3,11 +3,18 @@ type FormItemProps = {
   label: string;
   labelName: string;
   options?: string[];
+  className: string;
 };
-const FormItem = ({ type, label, labelName, options }: FormItemProps) => {
+const FormItem = ({
+  type,
+  label,
+  labelName,
+  options,
+  className,
+}: FormItemProps) => {
   if (type == "select") {
     return (
-      <div className="sm:col-span-1">
+      <div className={className}>
         <label
           htmlFor={label}
           className="block text-sm font-medium leading-6 text-gray-900"
@@ -19,14 +26,33 @@ const FormItem = ({ type, label, labelName, options }: FormItemProps) => {
             id={label}
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-picton-blue-500 sm:max-w-xs sm:text-sm sm:leading-6"
           >
-            {options?.map((item) => <option>{item}</option>)}
+            {options?.map((item) => <option key={item}>{item}</option>)}
           </select>
         </div>
       </div>
     );
+  } else if (type == "textarea") {
+    return (
+      <div className={className}>
+        <label
+          htmlFor={label}
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          {labelName}
+        </label>
+        <div className="mt-2">
+          <textarea
+            rows={3}
+            required
+            id={label}
+            className="resize-none block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-picton-blue-500 sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+    )
   } else {
     return (
-      <div className="sm:col-span-1">
+      <div className={className}>
         <label
           htmlFor={label}
           className="block text-sm font-medium leading-6 text-gray-900"
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,35 +1,73 @@
 import Head from "next/head";
 import FormItem from "~/lib/components/FormItem";
 
-const contact = (props) => {
+const contact = () => {
   return (
     <>
       <Head>
         <title>Contact</title>
       </Head>{" "}
-      <div className="max-w-[1000px] m-5 bg-gray-100 p-5 self-center">
-      <form>
-        <div className="space-y-12">
-          <div className="border-b border-gray-900/10 pb-12">
-            <h2 className="text-base font-semibold leading-7 text-picton-blue-500">
-              Contact
-            </h2>
-            <p className="mt-1 text-sm leading-6 text-gray-600">
-              Contacte nous !
-            </p>
+      <div className="flex h-full min-h-max w-full flex-col items-center justify-center pt-16">
+        <div className="radius m-5 w-full max-w-[1000px] bg-gray-100 p-5">
+          <form>
+            <div className="space-y-12">
+              <div className="border-b border-gray-900/10 pb-12">
+                <h2 className="text-base font-semibold leading-7 text-picton-blue-500">
+                  Contact
+                </h2>
+                <p className="mt-1 text-sm leading-6 text-gray-600">
+                  Contacte nous !
+                </p>
 
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <FormItem label="name" type="text" labelName="Nom et prénom" />
-              <FormItem
-                label="subject"
-                type="select"
-                labelName="Selection"
-                options={["pizza", "pasta"]}
-              />
+                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                  <FormItem
+                    className="sm:col-span-3"
+                    label="name"
+                    type="text"
+                    labelName="Nom"
+                  />
+                  <FormItem
+                    className="sm:col-span-3"
+                    label="name"
+                    type="text"
+                    labelName="Prénom"
+                  />
+                  <FormItem
+                    className="sm:col-span-6"
+                    label="name"
+                    type="email"
+                    labelName="Adresse email"
+                  />
+                  <FormItem
+                    className="sm:col-span-6"
+                    label="subject"
+                    type="select"
+                    labelName="Sujet"
+                    options={[
+                      "Information",
+                      "Compétition",
+                      "Entraînement",
+                      "Site internet",
+                      "Autre",
+                    ]}
+                  />
+                  <FormItem
+                    className="sm:col-span-6"
+                    label="message"
+                    type="textarea"
+                    labelName="Message"
+                  />
+                  <button
+                    type="submit"
+                    className="sm:col-span-6 rounded-md bg-picton-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-picton-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Envoyer
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
-      </form>
       </div>
     </>
   );
