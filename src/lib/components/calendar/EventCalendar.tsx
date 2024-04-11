6ee762377ffee86e34a27e48f860e0f134c299d6
dieu-antony <antonydieu@outlook.com>
2024-04-11 20:05:35 +0200

    feat: add dialog and tooltip on calendar

--- src/lib/components/calendar/EventCalendar.tsx
+++ src/lib/components/calendar/EventCalendar.tsx
@@ -10,15 +10,31 @@ import {
 } from "date-fns";
 import { Button } from "../ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import { CalendarIcon } from "lucide-react";
+import { CalendarIcon, MapPin } from "lucide-react";
 import { Calendar } from "../ui/calendar";
 import { cn } from "~/lib/utils";
 import { inter } from "~/pages/_app";
 import clsx from "clsx";
 import { CalendarEvent } from "@prisma/client";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+import { Clock8 } from "lucide-react";
 
-
-const EventCalendar = ({ events }:{events: CalendarEvent[]}) => {
+const EventCalendar = ({ events }: { events: CalendarEvent[] }) => {
   const [chosenMonth, setChosenMonth] = useState<Date>(new Date());
   const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   const firstDayOfMonth = startOfMonth(chosenMonth);
@@ -29,7 +45,6 @@ const EventCalendar = ({ events }:{events: CalendarEvent[]}) => {
   });
   const startingDayIndex =
     getDay(firstDayOfMonth) - 1 === -1 ? 6 : getDay(firstDayOfMonth) - 1;
-  console.log(startingDayIndex);
 
   const previousMonth = (chosenMonth: Date) => {
     return new Date(chosenMonth.getFullYear(), chosenMonth.getMonth() - 1);
@@ -99,7 +114,7 @@ const EventCalendar = ({ events }:{events: CalendarEvent[]}) => {
         })}
         {Array.from({ length: startingDayIndex }).map((_, index) => {
           return (
-            <div className="bg-gray-400 text-center" key={index}>
+            <div className="bg-gray-400 text-center border" key={index}>
               {format(
                 daysInPreviousMonth[startingDayIndex - 1 - index]?.toString() ||
                   new Date().toString(),
@@ -112,28 +127,90 @@ const EventCalendar = ({ events }:{events: CalendarEvent[]}) => {
           return (
             <div
               key={day.toString()}
-              className={clsx("text-center", {
+              className={clsx("text-center border", {
                 "bg-picton-blue-500": isToday(day),
               })}
             >
               {format(day.toString(), "d")}
               {events
-                  .filter((event) => isSameDay(event.start, day))
-                  .map((event) => {
-                    return <div key={event.summary}>{event.summary}</div>;
-                  })}
+                .filter((event) => isSameDay(event.start, day))
+                .map((event) => {
+                  return (
+                    <div
+                      key={event.summary}
+                      className={`font-sans ${inter.variable} bg-picton-blue-500 text-white p-1 rounded-md text-nowrap`}
+                    >
+                      <AlertDialog>
+                        <AlertDialogTrigger>
+                          <TooltipProvider>
+                            <Tooltip>
+                              <TooltipTrigger>
+                                <div
+                                  className={`flex font-sans ${inter.variable}`}
+                                >
+                                  {event.summary}
+                                </div>
+                              </TooltipTrigger>
+                              <TooltipContent>
+                                <div
+                                  className={`flex font-sans ${inter.variable} gap-1`}
+                                >
+                                  <div>{event.summary}</div>
+                                  <div>({format(event.start, "HH:mm")})</div>
+                                </div>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                          <AlertDialogHeader>
+                            <AlertDialogTitle>
+                              <div
+                                className={`flex font-sans ${inter.variable}`}
+                              >
+                                {event.summary}
+                              </div>
+                            </AlertDialogTitle>
+                            <AlertDialogDescription>
+                              <div className="flex flex-col gap-1">
+                                <div
+                                  className={`flex font-sans ${inter.variable} gap-1`}
+                                >
+                                  <Clock8 className="size-5" />{format(event.start, "dd.MM.yyyy")}{": "}
+                                  {format(event.start, "HH:mm")} -{" "}
+                                  {format(event.end, "HH:mm")}
+                                </div>
+                                <div
+                                  className={`flex font-sans ${inter.variable} gap-1`}
+                                >
+                                  <MapPin className="size-5" /> {event.location}
+                                </div>
+                              </div>
+                            </AlertDialogDescription>
+                          </AlertDialogHeader>
+                          <AlertDialogFooter>
+                            <AlertDialogCancel>
+                              <p className={`font-sans ${inter.variable}`}>
+                                Close
+                              </p>
+                            </AlertDialogCancel>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    </div>
+                  );
+                })}
             </div>
           );
         })}
         {Array.from({ length: 35 - startingDayIndex - daysInMonth.length }).map(
           (_, index) => {
             return (
-              <div className="bg-gray-400 text-center" key={index}>
+              <div className="bg-gray-400 text-center border" key={index}>
                 {format(
                   daysInNextMonth[index]?.toString() || new Date().toString(),
                   "d",
                 )}
-                
               </div>
             );
           },
